13c7942f95341223146537fc5cecb7cc3d92161e
log-info <yhc944@gmail.com>
2022-05-06 21:04:05 +0900

    custom hook

---
+++ src/hooks/useInputs.js
@@ -0,0 +1,25 @@
+import { useCallback, useReducer } from "react";
+
+function reduce(state, action) {
+    switch (action.type) {
+        case 'CHANGE':
+            const { name, value } = action.change
+            return { ...state, [name]: value }
+        case 'RESET':
+            return action.initialInputs
+    }
+}
+
+function useInputs(initialInputs) {
+    console.log('initialInputs: ' + JSON.stringify({...initialInputs}))
+    const [ state, dispatch ] = useReducer(reduce, initialInputs)
+    const onChange = useCallback(e => {
+        const { name, value } = e.target
+        dispatch({ type: 'CHANGE', change: { name, value } })
+    }, [])
+    const reset = useCallback(() => dispatch({ type: 'RESET', initialInputs: initialInputs }))
+    console.log('state:' + JSON.stringify(state))
+    return [ state, onChange, reset ]
+}
+
+export default useInputs
--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -1,18 +1,41 @@
 import React, { useCallback, useMemo, useReducer, useRef } from 'react'
+import useInputs from '../hooks/useInputs';
 import CreateUser from "./CreateUser";
 import UserList from "./UserList";
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는 중...')
-  console.log(JSON.stringify(users))
   return users.filter(user => user.active).length
 }
-
-const defaultUser = {
-    username: "익명",
-    email: "@",
-    active: false
+  
+function reduce(state, action) {
+  const { type } = action
+  const { users } = state
+  switch (type) {
+    case 'CREATE_USER':
+      const {newUser, resetInput} = action
+      resetInput()
+      return {
+        users: users.concat(newUser),
+      }
+    case 'DELETE_USER':
+      const { deletedUserId } = action
+      return {
+        users: users.filter(e => e.id !== deletedUserId)
+      }
+    case 'TOGGLE_USER':
+      const { toggledUserId } = action
+      return {
+        users: users.map((user) => {
+          if (user.id == toggledUserId) {
+            return { ...user, active: !user.active }
+          }
+          return user
+        })
+      }
   }
+  return state
+}
   
   const defaultUsers = [
     {
@@ -36,68 +59,26 @@ const defaultUser = {
   ];
 
 function UserDashboard() {
-    const [state, dispatch] = useReducer(reduce, { user: defaultUser, users: defaultUsers })
-    const { users, user } = state
+    const [state, dispatch] = useReducer(reduce, { users: defaultUsers })
+    const { users } = state
     const nextId = useRef(4)
-    const { username, email } = user;
     const count = useMemo(() => countActiveUsers(users), [users]);
-  
-    function reduce(state, action) {
-      const { type } = action
-      const { user, users } = state
-      switch (type) {
-        case 'CHANGE_INPUT':
-          const { name, value } = action
-          return {
-            user: {
-              ...state.user,
-              [name]: value
-            },
-            users: users,
-          }
-        case 'CREATE_USER':
-          const {newUser} = action
-          return {
-            user: defaultUser,
-            users: users.concat(newUser),
-          }
-        case 'DELETE_USER':
-          const { deletedUserId } = action
-          return {
-            user: user,
-            users: users.filter(e => e.id !== deletedUserId)
-          }
-        case 'TOGGLE_USER':
-          const { toggledUserId } = action
-          console.log('toggledUserId: ' + toggledUserId)
-          return {
-            user: user,
-            users: users.map((user) => {
-              if (user.id == toggledUserId) {
-                return { ...user, active: !user.active }
-              }
-              return user
-            })
-          }
-      }
-      return state
-    }
-  
-    const onChange = useCallback(({ target }) => {
-      const { name, value } = target
-      dispatch({ type: 'CHANGE_INPUT', name: name, value: value })
-    }, [])
-  
+
+    const [inputs, onChange, reset] = useInputs({username:'test', email:'test@test.test'})
+    const resetInput = reset
+    console.log('inputs: ' + JSON.stringify(useInputs({username:'', email:''})))
+    const inputUserName = inputs.username
+    const inputEmail = inputs.email
   
     const onCreate = useCallback(() => {
       const newUser = {
         id: nextId.current++,
-        username: username,
-        email: email,
+        username: inputUserName,
+        email: inputEmail,
         active: false,
       }
-      dispatch({ type: 'CREATE_USER', newUser: newUser })
-    }, [username, email])
+      dispatch({ type: 'CREATE_USER', newUser: newUser, resetInput: resetInput })
+    }, [inputUserName, inputEmail])
   
     const onDelete = useCallback((userId) => {
       dispatch({ type: 'DELETE_USER', deletedUserId: userId })
@@ -110,7 +91,7 @@ function UserDashboard() {
     return (
         <>
             <h2>Array 렌더링</h2>
-            <CreateUser user={user} onChange={onChange} onCreate={onCreate} />
+            <CreateUser user={inputs} onChange={onChange} onCreate={onCreate} />
             <UserList users={users} onDelete={onDelete} onToggle={onToggle} />
             <div>활성 사용자 수: {count}</div>
         </>
